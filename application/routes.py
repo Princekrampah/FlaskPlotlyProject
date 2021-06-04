49580fa5705b8a2cb8a548f19fe533f90ef42501
Prince Krampah <jsksprince@gmail.com>
2021-06-04 18:33:47 +0300

    Project completed

---
+++ application/routes.py
@@ -0,0 +1,34 @@
+from application import app
+from flask import render_template
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chart1')
+def chart1():
+
+    # Graph One
+    df = px.data.medals_wide()
+    fig1 = px.bar(df, x="nation", y=["gold", "silver", "bronze"], title="Wide-Form Input")
+    graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph two
+    df = px.data.iris()
+    fig2 = px.scatter_3d(df, x='sepal_length', y='sepal_width', z='petal_width',
+              color='species',  title="Iris Dataset")
+    graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+    # Graph three
+    df = px.data.gapminder().query("continent=='Oceania'")
+    fig3 = px.line(df, x="year", y="lifeExp", color='country',  title="Life Expectancy")
+    graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+
+    return render_template('index.html', graph1JSON=graph1JSON,  graph2JSON=graph2JSON, graph3JSON=graph3JSON)
+
